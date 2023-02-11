fd9fdd6530669269f7c96647b0cbdfd03e3cac5d
hamzaR <hamza.rebb@gmail.com>
2023-02-11 19:02:30 +0100

    style is finished

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './App.css';
 
 interface Props {
   initialName: string;
@@ -19,27 +20,30 @@ const Card: React.FC<Props> = ({ initialName, initialStatus }) => {
   };
 
   return (
-    <div>
-      <img src="https://via.placeholder.com/150" alt="Profile" />
+    <div className='formBox'>
       {isEditing ? (
         <>
           <input
             type="text"
+            className='input'
             value={name}
+            placeholder="Enter your name"
             onChange={(event) => setName(event.target.value)}
           />
           <input
             type="text"
+            className='input secondaryInput'
             value={status}
+            placeholder="What's your situation?"
             onChange={(event) => setStatus(event.target.value)}
           />
-          <button onClick={handleSave}>Save</button>
+          <button className='myButton' onClick={handleSave}>Save</button>
         </>
       ) : (
         <>
           <div>{name}</div>
           <div>{status}</div>
-          <button onClick={handleEdit}>Edit</button>
+          <button className='myButton' onClick={handleEdit}>Edit</button>
         </>
       )}
     </div>
